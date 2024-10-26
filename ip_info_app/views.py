f2f02e487ea3f323e5e3c8838aa66e6eadc2071f
vetrof <mrtventana@gmail.com>
2024-10-26 16:56:30 +0300

    IpInfo13

--- ip_info_app/views.py
+++ ip_info_app/views.py
@@ -1,10 +1,12 @@
+import csv
+from ipware import get_client_ip
 from django.shortcuts import render
 from rest_framework.response import Response
+from rest_framework.utils import json
 from rest_framework.views import APIView
 import requests
 import ipinfo
 from django.conf import settings
-
 from ip_info_app.servises.api_info_handler import api_info_handler
 
 
@@ -18,11 +20,25 @@ class InfoMain(APIView):
         return Response(json_answer)
 
 
-#Добавить приложение history и сохранять историю отправки адресов и сохранить все ip
-#сОХРАНЯТЬ историю запросов конкретного человека и реализовать эндпоинт, который будет показывать эту историю
+# Добавить приложение history и сохранять историю отправки адресов и сохранить все ip
+# сОХРАНЯТЬ историю запросов конкретного человека и реализовать эндпоинт, который будет показывать эту историю
+# Добавить redis, хранить историю в redis запроса один час
+# почитать как делать кэш для эндпоинтов, сначала автоматический кэш, затем ручной
 class ForeingMain(APIView):
 
     def post(self, request):
         ip_adress = request.data.get("ip")
         json_answer = api_info_handler(ip_adress)
         return Response(json_answer)
+
+
+class HistoryMain(APIView):
+
+    def post(self, request):
+        ip_adress = request.data.get("ip")
+        json_answer = api_info_handler(ip_adress)
+        text = {"json_answer": json_answer}
+        with open('ip.csv', 'w') as file:
+            file.write(json.dumps(text))
+        return Response(json_answer)
+
